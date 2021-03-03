3b4955adf64b493662eb08a71c857e2d975035a8
Firoz <khan3firoz@gmail.com>
2021-03-03 10:44:57 +0530

    initial commite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnChanges{
   title = 'lifeCycleHooks';
+  constructor() { }
+  fontColor = 'blue';
+  sayHelloId = 78;
+  canClick = true;
+  message = 'Hello, World';
+  sayMessage() {
+      alert(this.message);
+  }
+  ngOnInit(): void{
+
+  }
+  ngOnChanges() {
+
+  }
 }
